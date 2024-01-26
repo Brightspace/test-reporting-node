90f24d8784a3626de5f59b0155cd29cef7ed3d3d
Devon Powell <Devon.Powell@D2L.com>
2024-01-26 14:23:35 -0500

    Add better report validation

--- test/integration/report-validation.test.js
+++ test/integration/report-validation.test.js
@@ -1,6 +1,11 @@
+import { expect, use } from 'chai';
+import chaiSubset from 'chai-subset';
+import { getOperatingSystem } from '../../src/reporters/helpers.cjs';
 import { hasContext } from '../../src/helpers/github.cjs';
 import { readFile } from 'node:fs/promises';
 import { validateReport } from '../../src/helpers/report.cjs';
+
+use(chaiSubset);
 
 const dummyContext = {
 	githubOrganization: 'TestOrganization',
@@ -11,35 +16,343 @@ const dummyContext = {
 	gitBranch: 'test/branch',
 	gitSha: '0000000000000000000000000000000000000000'
 };
+const partialReportMocha = {
+	summary: {
+		framework: 'mocha',
+		countPassed: 2,
+		countFailed: 2,
+		countSkipped: 2,
+		countFlaky: 2
+	},
+	details: [{
+		name: 'reporter tests 1 > test',
+		status: 'passed',
+		location: 'test/integration/mocha-1.test.js',
+		tool: 'Mocha 1 Test Reporting',
+		experience: 'Test Framework',
+		type: 'ui',
+		retries: 0
+	}, {
+		name: 'reporter tests 1 > skipped test',
+		status: 'skipped',
+		location: 'test/integration/mocha-1.test.js',
+		tool: 'Mocha 1 Test Reporting',
+		experience: 'Test Framework',
+		type: 'ui',
+		retries: 0
+	}, {
+		name: 'reporter tests 1 > flaky test that succeeds eventually',
+		status: 'passed',
+		location: 'test/integration/mocha-1.test.js',
+		tool: 'Mocha 1 Test Reporting',
+		experience: 'Test Framework',
+		type: 'ui',
+		retries: 2
+	}, {
+		name: 'reporter tests 1 > failed test',
+		status: 'failed',
+		location: 'test/integration/mocha-1.test.js',
+		tool: 'Mocha 1 Test Reporting',
+		experience: 'Test Framework',
+		type: 'ui',
+		retries: 3
+	}, {
+		name: 'reporter tests 2 > test',
+		status: 'passed',
+		location: 'test/integration/mocha-2.test.js',
+		tool: 'Test Reporting',
+		experience: 'Mocha 2 Test Framework',
+		type: 'integration',
+		retries: 0
+	}, {
+		name: 'reporter tests 2 > skipped test',
+		status: 'skipped',
+		location: 'test/integration/mocha-2.test.js',
+		tool: 'Test Reporting',
+		experience: 'Mocha 2 Test Framework',
+		type: 'integration',
+		retries: 0
+	}, {
+		name: 'reporter tests 2 > flaky test',
+		status: 'passed',
+		location: 'test/integration/mocha-2.test.js',
+		tool: 'Test Reporting',
+		experience: 'Mocha 2 Test Framework',
+		type: 'integration',
+		retries: 2
+	}, {
+		name: 'reporter tests 2 > failed test',
+		status: 'failed',
+		location: 'test/integration/mocha-2.test.js',
+		tool: 'Test Reporting',
+		experience: 'Mocha 2 Test Framework',
+		type: 'integration',
+		retries: 3
+	}]
+};
+const partialReportPlaywright = {
+	summary: {
+		framework: 'playwright',
+		countPassed: 6,
+		countFailed: 6,
+		countSkipped: 6,
+		countFlaky: 6
+	},
+	details: [{
+		name: '[chromium] > reporter tests 1 > skipped test',
+		status: 'skipped',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'chromium',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 0
+	}, {
+		name: '[chromium] > reporter tests 2 > skipped test',
+		status: 'skipped',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'chromium',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 0
+	}, {
+		name: '[firefox] > reporter tests 1 > skipped test',
+		status: 'skipped',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'firefox',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 0
+	}, {
+		name: '[chromium] > reporter tests 1 > test',
+		status: 'passed',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'chromium',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 0
+	}, {
+		name: '[chromium] > reporter tests 1 > failed test',
+		status: 'failed',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'chromium',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 3
+	}, {
+		name: '[chromium] > reporter tests 2 > test',
+		status: 'passed',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'chromium',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 0
+	}, {
+		name: '[firefox] > reporter tests 1 > test',
+		status: 'passed',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'firefox',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 0
+	}, {
+		name: '[firefox] > reporter tests 2 > skipped test',
+		status: 'skipped',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'firefox',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 0
+	}, {
+		name: '[chromium] > reporter tests 2 > failed test',
+		status: 'failed',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'chromium',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 3
+	}, {
+		name: '[firefox] > reporter tests 1 > failed test',
+		status: 'failed',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'firefox',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 3
+	}, {
+		name: '[firefox] > reporter tests 2 > test',
+		status: 'passed',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'firefox',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 0
+	}, {
+		name: '[firefox] > reporter tests 2 > failed test',
+		status: 'failed',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'firefox',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 3
+	}, {
+		name: '[chromium] > reporter tests 2 > flaky test',
+		status: 'passed',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'chromium',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 2
+	}, {
+		name: '[chromium] > reporter tests 1 > flaky test',
+		status: 'passed',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'chromium',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 2
+	}, {
+		name: '[webkit] > reporter tests 1 > skipped test',
+		status: 'skipped',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'webkit',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 0
+	}, {
+		name: '[firefox] > reporter tests 1 > flaky test',
+		status: 'passed',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'firefox',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 2
+	}, {
+		name: '[webkit] > reporter tests 1 > test',
+		status: 'passed',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'webkit',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 0
+	}, {
+		name: '[webkit] > reporter tests 2 > skipped test',
+		status: 'skipped',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'webkit',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 0
+	}, {
+		name: '[firefox] > reporter tests 2 > flaky test',
+		status: 'passed',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'firefox',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 2
+	}, {
+		name: '[webkit] > reporter tests 1 > failed test',
+		status: 'failed',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'webkit',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 3
+	}, {
+		name: '[webkit] > reporter tests 2 > test',
+		status: 'passed',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'webkit',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 0
+	}, {
+		name: '[webkit] > reporter tests 2 > failed test',
+		status: 'failed',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'webkit',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 3
+	}, {
+		name: '[webkit] > reporter tests 1 > flaky test',
+		status: 'passed',
+		location: 'test/integration/playwright-1.test.js',
+		browser: 'webkit',
+		tool: 'Playwright 1 Test Reporting',
+		experience: 'Playwright 1 Test Framework',
+		type: 'integration',
+		retries: 2
+	}, {
+		name: '[webkit] > reporter tests 2 > flaky test',
+		status: 'passed',
+		location: 'test/integration/playwright-2.test.js',
+		browser: 'webkit',
+		tool: 'Test Reporting',
+		experience: 'Playwright 2 Test Framework',
+		type: 'visual diff',
+		retries: 2
+	}]
+};
+const reportTests = [{
+	name: 'mocha',
+	path: './d2l-test-report-mocha.json',
+	expected: partialReportMocha
+}, {
+	name: 'playwright',
+	path: './d2l-test-report-playwright.json',
+	expected: partialReportPlaywright
+}];
 
 describe('report validation', () => {
-	it('mocha', async() => {
-		let report = await readFile('./d2l-test-report-mocha.json', 'utf8');
+	for (const reportTest of reportTests) {
+		describe(reportTest.name, () => {
+			let report;
 
-		report = JSON.parse(report);
+			before(async() => {
+				const reportFileContents = await readFile(reportTest.path, 'utf8');
+				const reportJson = JSON.parse(reportFileContents);
 
-		if (!hasContext()) {
-			report.summary = {
-				...report.summary,
-				...dummyContext
-			};
-		}
+				if (!hasContext()) {
+					reportJson.summary = {
+						...reportJson.summary,
+						...dummyContext
+					};
+				}
 
-		validateReport(report);
-	});
+				report = reportJson;
+			});
 
-	it('playwright', async() => {
-		let report = await readFile('./d2l-test-report-playwright.json', 'utf8');
+			it('schema', () => validateReport(report));
 
-		report = JSON.parse(report);
-
-		if (!hasContext()) {
-			report.summary = {
-				...report.summary,
-				...dummyContext
-			};
-		}
-
-		validateReport(report);
-	});
+			it('contents', () => {
+				expect(report.reportVersion).to.eq(1);
+				expect(report.summary.status).to.eq('failed');
+				expect(report.summary.operatingSystem).to.eq(getOperatingSystem());
+				expect(report).to.containSubset(reportTest.expected);
+			});
+		});
+	}
 });
