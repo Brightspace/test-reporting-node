6bd72ca0e99a0d5381c4aecc536a84473337fae2
Devon Powell <Devon.Powell@D2L.com>
2024-01-26 16:25:33 -0500

    Move around some integration test stuff

--- test/integration/data/config/web-test-runner.js
+++ test/integration/data/config/web-test-runner.js
@@ -1,19 +1,19 @@
 import { defaultReporter } from '@web/test-runner';
 import { playwrightLauncher } from '@web/test-runner-playwright';
-import { reporter } from '../../../src/reporters/web-test-runner.js';
+import { reporter } from '../../../../src/reporters/web-test-runner.js';
 
 export default {
 	reporters: [
 		defaultReporter(),
 		reporter({
 			reportPath: './d2l-test-report-web-test-runner.json',
-			reportConfigurationPath: './test/integration/configs/d2l-test-reporting.config.json'
+			reportConfigurationPath: './test/integration/data/config/d2l-test-reporting.json'
 		})
 	],
-	files: 'test/integration/web-test-runner-1.test.js',
+	files: 'test/integration/data/web-test-runner-1.test.js',
 	groups: [{
 		name: 'group 1',
-		files: 'test/integration/web-test-runner-2.test.js',
+		files: 'test/integration/data/web-test-runner-2.test.js',
 		browsers: [
 			playwrightLauncher({ product: 'chromium' }),
 			playwrightLauncher({ product: 'firefox' })
