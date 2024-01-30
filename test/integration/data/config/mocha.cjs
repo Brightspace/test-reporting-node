5c485e8067ddc108c3faa3f4b8e46e2550bc96f7
Devon Powell <Devon.Powell@D2L.com>
2024-01-30 12:02:23 -0500

    fix: add documentation on reporter usage (#81)

--- test/integration/data/config/mocha.cjs
+++ test/integration/data/config/mocha.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	spec: 'test/integration/data/mocha-*.test.js',
 	retries: 3,
 	reporter: 'src/reporters/mocha.cjs',
-	'reporter-option': [
+	reporterOptions: [
 		'reportPath=./d2l-test-report-mocha.json',
 		'reportConfigurationPath=./test/integration/data/config/d2l-test-reporting.json'
 	]
