414132a3c1a60cc5dca636d56509e3daa4adc5c1
Devon Powell <Devon.Powell@D2L.com>
2024-02-06 16:10:04 -0500

    fix: update to use purely json schema constructs

--- src/helpers/schema.cjs
+++ src/helpers/schema.cjs
@@ -1,57 +1,122 @@
-const Ajv = require('ajv');
+const Ajv = require('ajv/dist/2019');
 const addFormats = require('ajv-formats');
 
-const ajv = new Ajv({ verbose: true });
-
-addFormats(ajv, ['date-time', 'uri', 'uuid']);
-
-const nonEmptyStringPattern = '^(?!\\s).+(?<!\\s)$';
-const gitHubPattern = '[A-Za-z0-9_.-]+';
-const contextProperties = {
-	githubOrganization: { type: 'string', pattern: gitHubPattern },
-	githubRepository: { type: 'string', pattern: gitHubPattern },
-	githubWorkflow: { type: 'string', pattern: nonEmptyStringPattern },
-	githubRunId: { type: 'integer', minimum: 0 },
-	githubRunAttempt: { type: 'integer', minimum: 1 },
-	gitBranch: { type: 'string', pattern: nonEmptyStringPattern },
-	gitSha: { type: 'string', pattern: '([A-Fa-f0-9]{40})' }
+const nonEmptyNoPaddingStringSchema = {
+	$id: '/test-reporting/non-empty-no-padding-string',
+	type: 'string',
+	minLength: 1,
+	pattern: '^(?!\\s).+(?<!\\s)$'
 };
 const contextSchema = {
-	type: 'object',
-	properties: contextProperties,
-	required: Object.keys(contextProperties)
-};
-const contextSchemaLoose = {
-	...contextSchema,
-	additionalProperties: true
-};
-const contextSchemaStrict = {
-	...contextSchema,
-	additionalProperties: false
-};
-const reportSchema = {
+	$id: '/test-reporting/context',
+	$defs: {
+		gitHubPattern: {
+			type: 'string',
+			pattern: '[A-Za-z0-9_.-]+'
+		}
+	},
 	type: 'object',
 	properties: {
-		reportId: { type: 'string', format: 'uuid' },
-		reportVersion: { type: 'integer', const: 1 },
+		githubOrganization: { $ref: '#/$defs/gitHubPattern' },
+		githubRepository: { $ref: '#/$defs/gitHubPattern' },
+		githubWorkflow: { $ref: '/test-reporting/non-empty-no-padding-string' },
+		githubRunId: {
+			type: 'integer',
+			minimum: 0
+		},
+		githubRunAttempt: {
+			type: 'integer',
+			minimum: 1
+		},
+		gitBranch: { $ref: '/test-reporting/non-empty-no-padding-string' },
+		gitSha: {
+			type: 'string',
+			minLength: 40,
+			maxLength: 40,
+			pattern: '[A-Fa-f0-9]+'
+		}
+	},
+	required: [
+		'githubOrganization',
+		'githubRepository',
+		'githubWorkflow',
+		'githubRunId',
+		'githubRunAttempt',
+		'gitBranch',
+		'gitSha'
+	]
+};
+const contextSchemaLoose = {
+	$id: '/test-reporting/context-loose',
+	$ref: '/test-reporting/context',
+	type: 'object',
+	unevaluatedProperties: true
+};
+const contextSchemaStrict = {
+	$id: '/test-reporting/context-strict',
+	$ref: '/test-reporting/context',
+	type: 'object',
+	unevaluatedProperties: false
+};
+const reportSchema = {
+	$id: '/test-reporting/report',
+	type: 'object',
+	properties: {
+		reportId: {
+			type: 'string',
+			format: 'uuid'
+		},
+		reportVersion: {
+			type: 'integer',
+			const: 1
+		},
 		summary: {
 			type: 'object',
+			$ref: '/test-reporting/context',
 			properties: {
-				...contextProperties,
-				lmsBuild: { type: 'string', pattern: '([0-9]{2}\\.){3}[0-9]{5}' },
-				lmsInstance: { type: 'string', format: 'uri' },
-				operatingSystem: { type: 'string', enum: ['windows', 'linux', 'mac'] },
-				framework: { type: 'string', pattern: nonEmptyStringPattern },
-				started: { type: 'string', format: 'date-time' },
-				totalDuration: { type: 'integer', minimum: 0 },
-				status: { type: 'string', enum: ['passed', 'failed'] },
-				countPassed: { type: 'integer', minimum: 0 },
-				countFailed: { type: 'integer', minimum: 0 },
-				countSkipped: { type: 'integer', minimum: 0 },
-				countFlaky: { type: 'integer', minimum: 0 }
+				lmsBuild: {
+					type: 'string',
+					pattern: '([0-9]{2}\\.){3}[0-9]{5}'
+				},
+				lmsInstance: {
+					type: 'string',
+					format: 'uri'
+				},
+				operatingSystem: {
+					type: 'string',
+					enum: ['windows', 'linux', 'mac']
+				},
+				framework: { $ref: '/test-reporting/non-empty-no-padding-string' },
+				started: {
+					type: 'string',
+					format: 'date-time'
+				},
+				totalDuration: {
+					type: 'integer',
+					minimum: 0
+				},
+				status: {
+					type: 'string',
+					enum: ['passed', 'failed']
+				},
+				countPassed: {
+					type: 'integer',
+					minimum: 0
+				},
+				countFailed: {
+					type: 'integer',
+					minimum: 0
+				},
+				countSkipped: {
+					type: 'integer',
+					minimum: 0
+				},
+				countFlaky: {
+					type: 'integer',
+					minimum: 0
+				}
 			},
 			required: [
-				...Object.keys(contextProperties),
 				'operatingSystem',
 				'framework',
 				'started',
@@ -62,24 +127,33 @@ const reportSchema = {
 				'countSkipped',
 				'countFlaky'
 			],
-			additionalProperties: false
+			unevaluatedProperties: false
 		},
 		details: {
 			type: 'array',
 			items: {
 				type: 'object',
 				properties: {
-					name: { type: 'string', pattern: nonEmptyStringPattern },
-					location: { type: 'string', pattern: nonEmptyStringPattern },
+					name: { $ref: '/test-reporting/non-empty-no-padding-string' },
+					location: { $ref: '/test-reporting/non-empty-no-padding-string' },
 					started: { type: 'string', format: 'date-time' },
-					duration: { type: 'integer', minimum: 0 },
-					totalDuration: { type: 'integer', minimum: 0 },
+					duration: {
+						type: 'integer',
+						minimum: 0
+					},
+					totalDuration: {
+						type: 'integer',
+						minimum: 0
+					},
 					status: { type: 'string', enum: ['passed', 'failed', 'skipped'] },
-					tool: { type: 'string', pattern: nonEmptyStringPattern },
-					experience: { type: 'string', pattern: nonEmptyStringPattern },
-					type: { type: 'string', pattern: nonEmptyStringPattern },
+					tool: { $ref: '/test-reporting/non-empty-no-padding-string' },
+					experience: { $ref: '/test-reporting/non-empty-no-padding-string' },
+					type: { $ref: '/test-reporting/non-empty-no-padding-string' },
 					browser: { type: 'string', enum: ['chromium', 'chrome', 'firefox', 'webkit'] },
-					retries: { type: 'integer', minimum: 0 }
+					retries: {
+						type: 'integer',
+						minimum: 0
+					}
 				},
 				required: [
 					'name',
@@ -91,7 +165,9 @@ const reportSchema = {
 					'retries'
 				],
 				additionalProperties: false
-			}
+			},
+			minItems: 1,
+			uniqueItems: true
 		}
 	},
 	required: [
@@ -103,9 +179,23 @@ const reportSchema = {
 	additionalProperties: false
 };
 
-const validateContextLooseAjv = ajv.compile(contextSchemaLoose);
-const validateContextStrictAjv = ajv.compile(contextSchemaStrict);
-const validateReportAjv = ajv.compile(reportSchema);
+const ajv = new Ajv({
+	verbose: true,
+	strict: true,
+	schemas: [
+		nonEmptyNoPaddingStringSchema,
+		contextSchema,
+		contextSchemaStrict,
+		contextSchemaLoose,
+		reportSchema
+	]
+});
+
+addFormats(ajv, ['date-time', 'uri', 'uuid']);
+
+const validateContextLooseAjv = ajv.getSchema('/test-reporting/context-loose');
+const validateContextStrictAjv = ajv.getSchema('/test-reporting/context-strict');
+const validateReportAjv = ajv.getSchema('/test-reporting/report');
 
 module.exports = {
 	ajv,
