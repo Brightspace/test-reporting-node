112146e07255bcc6b23a9397381fe39a6d002353
Devon Powell <Devon.Powell@D2L.com>
2026-05-26 09:42:54 -0400

    fix: nest taxonomy and config in report schema v3 (attempt 2) (#845)

--- test/integration/data/validation/test-report-webdriverio.js
+++ test/integration/data/validation/test-report-webdriverio.js
@@ -15,137 +15,103 @@ export const testReportLatestPartial = {
 		name: `[${platform}] > reporter 1 > passed`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-1.test.js' },
-		tool: 'WebdriverIO 1 Test Reporting',
-		experience: 'Test Framework',
-		type: 'ui',
+		taxonomy: { tool: 'WebdriverIO 1 Test Reporting', type: 'ui' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 1 > skipped`,
 		status: 'skipped',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-1.test.js' },
-		tool: 'WebdriverIO 1 Test Reporting',
-		experience: 'Test Framework',
-		type: 'ui',
+		taxonomy: { tool: 'WebdriverIO 1 Test Reporting', type: 'ui' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 1 > flaky`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-1.test.js' },
-		tool: 'WebdriverIO 1 Test Reporting',
-		experience: 'Test Framework',
-		type: 'ui',
+		taxonomy: { tool: 'WebdriverIO 1 Test Reporting', type: 'ui' },
 		retries: 2
 	}, {
 		name: `[${platform}] > reporter 1 > failed`,
 		status: 'failed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-1.test.js' },
-		tool: 'WebdriverIO 1 Test Reporting',
-		experience: 'Test Framework',
-		type: 'ui',
+		taxonomy: { tool: 'WebdriverIO 1 Test Reporting', type: 'ui' },
 		retries: 3
 	}, {
 		name: `[${platform}] > reporter 1 > special/characters "(\\n\\r\\t\\b\\f)"`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-1.test.js' },
-		tool: 'WebdriverIO 1 Test Reporting',
-		experience: 'Test Framework',
-		type: 'ui',
+		taxonomy: { tool: 'WebdriverIO 1 Test Reporting', type: 'ui' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 2 > passed`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-2.test.js' },
-		tool: 'Test Reporting',
-		experience: 'WebdriverIO 2 Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 2 > skipped`,
 		status: 'skipped',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-2.test.js' },
-		tool: 'Test Reporting',
-		experience: 'WebdriverIO 2 Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 2 > flaky`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-2.test.js' },
-		tool: 'Test Reporting',
-		experience: 'WebdriverIO 2 Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 2
 	}, {
 		name: `[${platform}] > reporter 2 > special/characters "(\\n\\r\\t\\b\\f)"`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-2.test.js' },
-		tool: 'Test Reporting',
-		experience: 'WebdriverIO 2 Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 2 > passed 2`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-2.test.js' },
-		tool: 'Test Reporting',
-		experience: 'WebdriverIO 2 Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 2 > passed 3`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-2.test.js' },
-		tool: 'Test Reporting',
-		experience: 'WebdriverIO 2 Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 2 > passed 4`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-2.test.js' },
-		tool: 'Test Reporting',
-		experience: 'WebdriverIO 2 Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 2 > passed with timeout`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-2.test.js' },
-		tool: 'Test Reporting',
-		experience: 'WebdriverIO 2 Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 3 > passed 1`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-3.test.js' },
-		tool: 'Test Reporting',
-		experience: 'Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 3 > passed 2`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-3.test.js' },
-		tool: 'Test Reporting',
-		experience: 'Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 3 > passed 3`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-3.test.js' },
-		tool: 'Test Reporting',
-		experience: 'Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}, {
 		name: `[${platform}] > reporter 3 > passed 4`,
 		status: 'passed',
 		location: { file: 'test/integration/data/tests/webdriverio/reporter-3.test.js' },
-		tool: 'Test Reporting',
-		experience: 'Test Framework',
-		type: 'integration',
+		taxonomy: { tool: 'Test Reporting', type: 'integration' },
 		retries: 0
 	}]
 };
